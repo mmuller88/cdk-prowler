175089f6c0966fbe2f08ed195fec65875ab2d843
mmuller88 <damadden88@googlemail.com>
2021-12-12 10:07:07 +0100

    fix: try publishing to pypi

--- .projenrc.js
+++ .projenrc.js
@@ -49,14 +49,14 @@ const project = new awscdk.AwsCdkConstructLibrary({
     'cdk',
   ],
   // publishToPypi: {
-
-  //   distName: 'cdk-prowler',
-  //   module: 'cdk_prowler',
-  // },
-  publishToNuget: {
-    dotNetNamespace: 'com.github.mmuller88',
-    packageId: 'com.github.mmuller88.cdkProwler',
+  python: {
+    distName: 'cdk-prowler',
+    module: 'cdk_prowler',
   },
+  // publishToNuget: {
+  //   dotNetNamespace: 'com.github.mmuller88',
+  //   packageId: 'com.github.mmuller88.cdkProwler',
+  // },
   readme: {
     contents: `[![NPM version](https://badge.fury.io/js/cdk-prowler.svg)](https://badge.fury.io/js/cdk-prowler)
 [![PyPI version](https://badge.fury.io/py/cdk-prowler.svg)](https://badge.fury.io/py/cdk-prowler)
